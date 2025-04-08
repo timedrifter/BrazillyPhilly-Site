38964bb2e6faea19ba41ba125f7330dfa573b07a
timedrifter <renatocferraz@gmail.com>
2025-04-07 23:13:35 -0400

    JS update

--- fakeReviewsStyled.js
+++ fakeReviewsStyled.js
@@ -1,77 +1,227 @@
-
-// ✅ Emoji Font Style Injection
-const styleTag = document.createElement('style');
-styleTag.textContent = `
-  body, .styled-btn, #quote-box {
-    font-family: 'Arial', 'Segoe UI Emoji', 'Apple Color Emoji', 'Noto Color Emoji', sans-serif !important;
-  }
-`;
-document.head.appendChild(styleTag);
-
-
-
-// Brazilly Philly Fake Review Generator — True Non-Repeating + Styled
+// 🔥 Brazilly Philly Fake Review Generator – Full Emoji Flavorverse Edition
 
 const fullReviewList = [
-  { text: "Listening to them convinced me to choose the red pill.", author: "Neo, Matrix Magazine" },
-  { text: "This sandwich slaps harder than my steak with a cast iron.", author: "Joshua Weissman, YouTube Kitchen Lab" },
-  { text: "Vote for them and all your dreams will come true.", author: "Pedro, Napoleon Dynamite Magazine" },
-  { text: "We'll have what they're having!", author: "Harry & Sally, Meet Movie Magazine" },
-  { text: "BURRAZZZZZILLYYYYY PHILLYYYYY!", author: "Thor, Sandwich Summoner" },
-  { text: "It’s giving... Michelin Star. But make it beef.", author: "Random Food Influencer, #sponsoredmaybe" },
-  { text: "I could do this all day… if the sandwich keeps coming.", author: "Captain America, Stars & Stripes Eats" },
-  { text: "Flavor so bold, it should be locked up.", author: "Michael Franzese, Made Man Menu Report" },
-  { text: "My Krabby Patty’s jealous.", author: "SpongeBob SquarePants, Undersea Eats Monthly" },
-  { text: "Tastes like freedom… and the tears of my haters.", author: "Eddie Murphy, Comedy King Quarterly" },
-  {"text": "It\u2019s over, Anakin \u2014 I have the high flavor.", "author": "Obi-Wan Kenobi, Star Wars Magazine"},
-  {"text": "I find your lack of Brazilly Philly... disturbing.", "author": "Darth Vader, Star Wars Weekly"},
-  {"text": "Mmm. Brazilly Philly, you must try. Strong with the spice, it is.", "author": "Yoda, Star Wars Digest"},
-  {"text": "I was just a farm boy... now I\u2019m a sandwich Jedi.", "author": "Luke Skywalker, Star Wars Magazine"},
-  {"text": "Illogical... yet delicious.", "author": "Spock, Star Trek Gazette"},
-  {"text": "I\u2019ve kissed green aliens, but I\u2019ve never had anything this hot.", "author": "Captain Kirk, Star Trek Quarterly"},
-  {"text": "Make it chew.", "author": "Picard, Star Trek Quarterly"},
-  {"text": "I cannot taste. But I can confirm \u2014 this sandwich achieves optimal structure.", "author": "Data, Star Trek Gazette"},
-  {"text": "This isn\u2019t just lunch. It\u2019s a mission.", "author": "Adama, Battlestar Galactica Circular"},
-  {"text": "We evolved... to taste this.", "author": "Cylon, Battlestar Galactica Circular"},
-  {"text": "You can\u2019t take the sky from me\u2026 but you can take this sandwich \u2014 over my dead body.", "author": "Malcolm Reynolds, Firefly Circular"},
-  {"text": "I tasted time. It was Brazilly Philly.", "author": "River Tam, Firefly Weekly"},
-  {"text": "I fight for freedom... and a bite of that grilled picanha glory.", "author": "Harlock, Captain Harlock Magazine"},
-  {"text": "Whatever happens, happens\u2026 but if I die, bury me with one of these.", "author": "Spike Spiegel, Cowboy Bebop Review"},
-  {"text": "I\u2019ve conned princes, but this sandwich conned me into love.", "author": "Faye Valentine, Cowboy Bebop Quarterly"},
-  {"text": "He who can taste this sandwich and remain still \u2014 he is truly the Kwisatz Haderach.", "author": "Paul Atreides, Dune Circular"},
-  {"text": "Your chefs come from noble blood... yet your kitchen smells of SPICE. The Brazilly Philly... is the test. Put your mouth in the bun.", "author": "Reverend Mother Mohiam, Dune Digest"},
-  {"text": "They have a wonderful kitchen, cousin... So rich. So tender. I could float forever.", "author": "Baron Harkonnen, Dune Gazette"},
-  {"text": "Why wasn\u2019t I told of this sandwich before?!", "author": "Beast Rabban, Dune Chronicles"},
-  {"text": "I grew potatoes on Mars. But Earth has Brazilly Philly. Game over.", "author": "Mark Watney, The Martian Review"},
-  {"text": "Best thing I\u2019ve tasted since dancing with an alien to \u201970s hits.", "author": "Star-Lord, Guardians of the Galaxy Chronicles"},
-  {"text": "I\u2026 am\u2026 HUNGRY.", "author": "Groot, Guardians of the Galaxy Weekly"},
-  {"text": "The only thing scarier than xenomorphs\u2026 is running out of Brazilly Philly.", "author": "Ripley, Alien Chronicles"},
-  {"text": "Hisss\u2026 crunch.", "author": "Xenomorph, Alien Quarterly"},
-  {"text": "We fought over water on Ceres. But for Brazilly Philly? We\u2019d start a new war.", "author": "James Holden, The Expanse Gazette"},
-  {"text": "I traveled to Neptune. Found nothing. Came back. Brazilly Philly was here all along.", "author": "Roy McBride, Ad Astra Gazette"},
-  {"text": "Eeeee-va... BRA-ZI-LYYYYYY.", "author": "WALL-E, WALL-E Review"},
-  {"text": "Directives updated: Retrieve sandwich.", "author": "EVE, WALL-E Quarterly"},
-  {"text": "If I die today, bury me inside a Brazilly Philly roll.", "author": "Gary Goodspeed, Final Space Quarterly"},
-  {"text": "I\u2019m doing my part \u2014 to devour every last bite.", "author": "Johnny Rico, Starship Troopers Magazine"},
-  {"text": "Never give up. Never surrender. And never skip the vinagrete.", "author": "Jason Nesmith, Galaxy Quest Review"},
-  {"text": "Treasure? Nah. This sandwich is the map.", "author": "Jim Hawkins, Treasure Planet Quarterly"},
-  {"text": "Good news, everyone! I discovered a sandwich that bends space\u2026 and time.", "author": "Professor Farnsworth, Futurama Digest"},
-  {"text": "Bite my shiny picanha!", "author": "Bender, Futurama Weekly"},
-  {"text": "Morty, you don\u2019t understand \u2014 the Brazilly Philly is the sandwich nexus of all realities.", "author": "Rick Sanchez, Rick and Morty Gazette"},
-  {"text": "Aw geez, Rick... it's so good I think I blacked out.", "author": "Morty Smith, Rick and Morty Gazette"},
-  {"text": "Danger, flavor overload!", "author": "Will Robinson, Lost in Space Chronicles"},
-  {"text": "Alone at the end of the world. At least I\u2019ve got this Brazilly Philly.", "author": "Augustine Lofthouse, The Midnight Sky Digest"},
-  {"text": "They should have sent... more sandwiches.", "author": "Dr. Ellie Arroway, Contact Review"},
-  {"text": "LUDICROUS FLAVOR!", "author": "Dark Helmet, Spaceballs Chronicles"},
-  {"text": "I\u2019m a Mog \u2014 half man, half dog \u2014 and ALL in on this sandwich.", "author": "Barf, Spaceballs Review"},
-  {"text": "Houston, we have no problem. We have Brazilly Philly.", "author": "Jim Lovell, Apollo 13 Gazette"}
+  {
+    "text": "Listening to them convinced me to choose the red pill.",
+    "author": "\ud83d\udd76\ufe0f Neo, Matrix Magazine"
+  },
+  {
+    "text": "See how far the rabbit hole goes.",
+    "author": "\ud83e\udde0 Morpheus, Matrix Magazine"
+  },
+  {
+    "text": "Vote for Brazilly Philly and all your dreams will come true.",
+    "author": "\ud83e\uddd4 Pedro, Napoleon Dynamite Magazine"
+  },
+  {
+    "text": "They wake up in the morning and piss excellence.",
+    "author": "\ud83c\udfce\ufe0f Ricky Bobby, Talladega Nights Digest"
+  },
+  {
+    "text": "We'll have what they're having!",
+    "author": "\ud83c\udf5d Harry & Sally, Meet Cute Circular"
+  },
+  {
+    "text": "I'm in a glass case of emotion!",
+    "author": "\ud83d\udcfa Ron Burgundy, Anchorman Weekly"
+  },
+  {
+    "text": "I recognize a one-man wolfpack when I see one.",
+    "author": "\ud83d\udc3a Alan, Hangover Magazine"
+  },
+  {
+    "text": "I've fallen and I can't get up.",
+    "author": "\ud83e\uddd3 Some old broad, Old People Magazine"
+  },
+  {
+    "text": "It's breathtaking.",
+    "author": "\ud83d\ude3c Dr. Evil, Virtucon Digest"
+  },
+  {
+    "text": "It makes me randy, baby.",
+    "author": "\ud83d\udd7a Austin Powers, Randy Magazine"
+  },
+  {
+    "text": "Say hello to my little friend.",
+    "author": "\ud83d\udca5 Tony Montana, Gangsta Review"
+  },
+  {
+    "text": "I only have a minute, but this sells itself.",
+    "author": "\ud83e\uddfd ShamWow Guy, Infomercial Network"
+  },
+  {
+    "text": "I'll be back.",
+    "author": "\ud83e\udd16 The Terminator, Time Travel Magazine"
+  },
+  {
+    "text": "It made my day.",
+    "author": "\ud83c\udfaf Clint Eastwood, Dirty Harry Digest"
+  },
+  {
+    "text": "It was better than Cats.",
+    "author": "\ud83c\udfad NYC Broadway Fans, NYC Theater Gazette"
+  },
+  {
+    "text": "Mikey likes it.",
+    "author": "\ud83e\udd63 Mikey's Big Brother, Life Cereal Digest"
+  },
+  {
+    "text": "We have a Hulk. But we also have picanha.",
+    "author": "\ud83e\udde0 Tony Stark, Stark Industries Catering Review"
+  },
+  {
+    "text": "I could do this all day\u2026 if the sandwich keeps coming.",
+    "author": "\ud83d\udee1\ufe0f Captain America, Stars & Stripes Eats"
+  },
+  {
+    "text": "This is the way... to flavor.",
+    "author": "\ud83d\udee1\ufe0f The Mandalorian, Helmet-Off Cuisine"
+  },
+  {
+    "text": "Set phasers to DELICIOUS.",
+    "author": "\ud83d\ude80 Captain Kirk, Enterprise Eats"
+  },
+  {
+    "text": "Resistance is futile\u2026 especially when you're hungry.",
+    "author": "\ud83d\udd0c The Borg, Star Trek Digest"
+  },
+  {
+    "text": "Great Scott! This thing\u2019s 1.21 gigaflavor!",
+    "author": "\u26a1 Doc Brown, Time Traveler\u2019s Cookbook"
+  },
+  {
+    "text": "I'd trade my last Infinity Stone for one more bite.",
+    "author": "\ud83e\ude90 Thanos, Titan Eats Quarterly"
+  },
+  {
+    "text": "The Force is strong with this one\u2026 especially the sauce.",
+    "author": "\ud83e\uddd9 Yoda, Tiny Food Critic Weekly"
+  },
+  {
+    "text": "You underestimate the power\u2026 of Brazilian steak.",
+    "author": "\ud83c\udf11 Darth Vader, Dark Side Deli Digest"
+  },
+  {
+    "text": "I came back from the future for this sandwich. Worth the flux capacitor.",
+    "author": "\ud83d\udef9 Marty McFly, Hover Review"
+  },
+  {
+    "text": "WUBBA LUBBA DELICIOUS!",
+    "author": "\ud83e\uddea Rick Sanchez, Food Portal Weekly"
+  },
+  {
+    "text": "It\u2019s so good, I stopped saying \u2018D\u2019oh!\u2019",
+    "author": "\ud83c\udf69 Homer Simpson, Donut Digest"
+  },
+  {
+    "text": "My Krabby Patty\u2019s jealous.",
+    "author": "\ud83c\udf4d SpongeBob SquarePants, Undersea Eats Monthly"
+  },
+  {
+    "text": "We want to pump... you up... with FLAVOR!",
+    "author": "\ud83d\udcaa Hans & Franz, SNL Protein Pack"
+  },
+  {
+    "text": "Well isn\u2019t that special?",
+    "author": "\ud83d\ude4f Church Lady, Sinfully Good Sandwich Digest"
+  },
+  {
+    "text": "It\u2019s party time. Excellent. Sandwich time. Double Excellent!",
+    "author": "\ud83c\udfb8 Wayne & Garth, Wayne\u2019s World Weekly"
+  },
+  {
+    "text": "We\u2019re on a mission from God. For this sandwich.",
+    "author": "\ud83c\udfb7 The Blues Brothers, Holy Bites Bulletin"
+  },
+  {
+    "text": "It\u2019s so good\u2026 I Chappelle\u2019d my pants.",
+    "author": "\ud83c\udfa4 Dave Chappelle, Stand-Up Digest"
+  },
+  {
+    "text": "This sandwich has more charisma than every Saved by the Bell character combined.",
+    "author": "\ud83d\udcfa Zack M., Time Out Champion"
+  },
+  {
+    "text": "I passed my GED right after eating it.",
+    "author": "\ud83e\udd16 Johnny 5, Robot Review Weekly"
+  },
+  {
+    "text": "This sandwich is SO hot right now.",
+    "author": "\ud83d\udc85 Mugatu, Zoolander Magazine"
+  },
+  {
+    "text": "This is the sandwich that will disrupt everything.",
+    "author": "\ud83d\udc68\u200d\ud83d\udcbb Richard Hendricks, Silicon Valley Stack Report"
+  },
+  {
+    "text": "It\u2019s giving... Michelin Star. But make it beef.",
+    "author": "\ud83d\udcf8 Random Food Influencer, #sponsoredmaybe"
+  },
+  {
+    "text": "This sandwich slaps harder than my steak with a cast iron.",
+    "author": "\ud83d\udc68\u200d\ud83c\udf73 Joshua Weissman, YouTube Kitchen Lab"
+  },
+  {
+    "text": "I tasted it, and then I said... BRUH. That\u2019s flavor.",
+    "author": "\ud83d\udd25 Mr. Make It Happen, Self-Taught Gourmet Digest"
+  },
+  {
+    "text": "You want food with soul? THIS is what I\u2019m talkin\u2019 about!",
+    "author": "\ud83c\udf57 Smokin' & Grillin' with AB, Backyard Boss Magazine"
+  },
+  {
+    "text": "It's not just another cooking show. It's another universe of taste.",
+    "author": "\ud83c\udfa5 Not Another Cooking Show, Grandma-Approved Gazette"
+  },
+  {
+    "text": "If this sandwich were a bassline, it'd groove nonstop.",
+    "author": "\ud83c\udfb8 Davie504, Slap It Sandwich Weekly"
+  },
+  {
+    "text": "The balance is golden. So is the sandwich.",
+    "author": "\u2696\ufe0f The Golden Balance, Halal Flavor Journal"
+  },
+  {
+    "text": "I\u2019d break bread with this flavor any day.",
+    "author": "\ud83c\udf7d\ufe0f Chef James Makinson, Culinary Chronicles"
+  },
+  {
+    "text": "You can taste the technique in every bite.",
+    "author": "\ud83d\udd2a Chef Billy Parisi, Knife Skills Monthly"
+  },
+  {
+    "text": "Yan can cook... but Brazilly Philly can SING!",
+    "author": "\ud83d\udc68\u200d\ud83c\udf73 Chef Martin Yan, East Meets Feast Review"
+  },
+  {
+    "text": "Flavor so bold, it should be locked up.",
+    "author": "\ud83d\udd74\ufe0f Michael Franzese, Made Man Menu Report"
+  },
+  {
+    "text": "If I don\u2019t yell \u2018LET'S GET IT\u2019 after eating, something\u2019s wrong.",
+    "author": "\ud83e\udd69 Guga Foods, Meat Science Monthly"
+  },
+  {
+    "text": "This food has... PASSION! EMOTION! DRAMA!",
+    "author": "\ud83c\uddeb\ud83c\uddf7 Chef Jean-Pierre, French American Finesse"
+  },
+  {
+    "text": "Even Uncle Roger would not be mad. Maybe.",
+    "author": "\ud83c\udf73 Uncle Roger, Rice Not Included Weekly"
+  },
+  {
+    "text": "Flavor so loud it woke my ancestors.",
+    "author": "\ud83c\udf36\ufe0f Cooking Con Claudia, Latino Food Legends"
+  },
+  {
+    "text": "This is the kind of thing that would make a grandma cry. In a good way.",
+    "author": "\ud83e\uddc1 Emma\u2019s Goodies, Dessert Divine Weekly"
+  }
 ];
 
-let shuffledIndex = shuffleIndexArray(fullReviewList.length);
-let currentIndex = 0;
+let reviewPool = [];
 
-function shuffleIndexArray(length) {
-  const array = Array.from({ length }, (_, i) => i);
+function shuffle(array) {
   for (let i = array.length - 1; i > 0; i--) {
     const j = Math.floor(Math.random() * (i + 1));
     [array[i], array[j]] = [array[j], array[i]];
@@ -80,28 +230,25 @@ function shuffleIndexArray(length) {
 }
 
 function generateReview() {
-  if (currentIndex >= shuffledIndex.length) {
-    // Only reshuffle AFTER the list is fully used
-    shuffledIndex = shuffleIndexArray(fullReviewList.length);
-    currentIndex = 0;
+  if (reviewPool.length === 0) {
+    reviewPool = shuffle([...fullReviewList]);
   }
 
-  const index = shuffledIndex[currentIndex++];
-  const next = fullReviewList[index];
+  const next = reviewPool.pop();
 
   const quoteBox = document.getElementById('quote-box');
   quoteBox.innerHTML = `
     <div style="
       background: #001933;
-      border: 2px solid #00aeef;
+      border: 2px solid #3399ff;
       border-radius: 16px;
       padding: 20px;
       max-width: 600px;
       text-align: center;
       color: #fff;
-      margin: 20px auto 10px;
-      font-family: 'Arial', sans-serif;
-      box-shadow: 0 0 25px #00aeef66;
+      margin: 40px auto;
+      font-family: 'Arial', 'Segoe UI Emoji', 'Apple Color Emoji', 'Noto Color Emoji', sans-serif;
+      box-shadow: 0 0 30px #3399ff80;
     ">
       <div style="font-size: 1.4rem; margin-bottom: 15px;">${next.text}</div>
       <div style="font-style: italic; font-size: 1rem;">— ${next.author}</div>
@@ -110,36 +257,29 @@ function generateReview() {
 }
 
 document.addEventListener('DOMContentLoaded', () => {
+  const styleTag = document.createElement('style');
+  styleTag.textContent = `
+    body, .styled-btn, #quote-box {
+      font-family: 'Arial', 'Segoe UI Emoji', 'Apple Color Emoji', 'Noto Color Emoji', sans-serif !important;
+    }
+  `;
+  document.head.appendChild(styleTag);
+
   const btn = document.getElementById('generate-btn');
   if (btn) {
-    btn.classList.add('styled-btn');
-    btn.addEventListener('click', generateReview);
-  }
-
-  const styleTag = document.createElement('style'); styleTag.textContent = `
-    body, .styled-btn, #quote-box {
-      font-family: 'Arial', 'Segoe UI Emoji', 'Apple Color Emoji', 'Noto Color Emoji', sans-serif;
-    }
-
-    .styled-btn {
+    btn.style.cssText = `
       padding: 12px 24px;
       font-size: 1rem;
-      background-color: #00aeef;
+      background-color: #3399ff;
       border: none;
       border-radius: 6px;
       color: #fff;
       font-weight: bold;
       cursor: pointer;
-      box-shadow: 0 0 10px rgba(0, 174, 239, 0.4);
-      transition: background 0.3s, transform 0.2s, box-shadow 0.3s;
-    }
-    .styled-btn:hover {
-      background-color: #008ec2;
-      transform: translateY(-2px);
-      box-shadow: 0 0 15px rgba(0, 174, 239, 0.6);
-    }
-  `;
-  document.head.appendChild(styleTag);
+      box-shadow: 0 0 10px rgba(51,153,255,0.5);
+    `;
+    btn.addEventListener('click', generateReview);
+  }
 
   generateReview();
-});
+};
